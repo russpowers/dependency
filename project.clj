dd9e51b1c569e9d954cbd4794cc3096826f88175
Russ Powers <russpowers@gmail.com>
2014-08-30 13:00:48 -0700

    Update dependencies

--- project.clj
+++ project.clj
@@ -1,22 +1,23 @@
-(defproject com.stuartsierra/dependency "0.1.2.x1-SNAPSHOT"
+(defproject com.stuartsierra/dependency "0.1.3-SNAPSHOT"
   :description "A data structure for representing dependency graphs"
   :url "https://github.com/stuartsierra/dependency"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojurescript "0.0-2197"]]
-  :profiles {:dev {:dependencies [[org.clojure/clojure "1.5.1"]]}
+  :dependencies [[org.clojure/clojurescript "0.0-2322"]]
+  :profiles {:dev {:dependencies [[org.clojure/clojure "1.6.0"]]}
+             :clj-1.5.0 {:dependencies [[org.clojure/clojure "1.5.0"]]}
              :clj-1.4.0 {:dependencies [[org.clojure/clojure "1.4.0"]]}
              :clj-1.3.0 {:dependencies [[org.clojure/clojure "1.3.0"]]}}
-  :plugins [[com.keminglabs/cljx "0.3.2" :exclusions [org.clojure/clojure]]
+  :plugins [[com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]
             [lein-cljsbuild "1.0.3"]
             [com.cemerick/clojurescript.test "0.3.0"]]
   :hooks [leiningen.cljsbuild cljx.hooks]
   :source-paths ["src" "target/gen/src"]
   :test-paths ["test" "target/gen/test"]
-  :cljx {:builds [{:source-paths ["src"]  :output-path "target/gen/src"  :rules :clj}
-                  {:source-paths ["src"]  :output-path "target/gen/src"  :rules :cljs}
-                  {:source-paths ["test"] :output-path "target/gen/test" :rules :clj}
-                  {:source-paths ["test"] :output-path "target/gen/test" :rules :cljs}]}
+  :cljx {:builds [{:source-paths ["src/"]  :output-path "target/gen/src"  :rules :clj}
+                  {:source-paths ["src/"]  :output-path "target/gen/src"  :rules :cljs}
+                  {:source-paths ["test/"] :output-path "target/gen/test" :rules :clj}
+                  {:source-paths ["test/"] :output-path "target/gen/test" :rules :cljs}]}
   :cljsbuild {:builds {:dev {:source-paths ["target/gen/src"]
                              :compiler {:output-to "target/gen/js/main.js"
                                         :output-dir "target/gen/js"
