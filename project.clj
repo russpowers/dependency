21eb783776d1f480ded6aa22c0c2eca2ca76e4ee
Russ Powers <russpowers@gmail.com>
2014-08-30 18:19:38 -0700

    Removed cljsbuild hook

--- project.clj
+++ project.clj
@@ -1,40 +1,42 @@
 (defproject org.clojars.russpowers/dependency "0.1.3-SNAPSHOT"
   :description "A data structure for representing dependency graphs"
-  :url "https://github.com/stuartsierra/dependency"
+  :url "https://github.com/russpowers/dependency"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojurescript "0.0-2322"]
                  [org.clojure/clojure "1.6.0"]]
-  :plugins [[com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]
-            [lein-cljsbuild "1.0.3"]]
-  :profiles {:dev 
-    {:plugins [[com.cemerick/clojurescript.test "0.3.1"]]}}
-  :hooks [leiningen.cljsbuild cljx.hooks]
-  :source-paths ["src" "target/gen/src"]
-  :test-paths ["test" "target/gen/test"]
-  :cljx {:builds [{:source-paths ["src"]  :output-path "target/gen/src"  :rules :clj}
-                  {:source-paths ["src"]  :output-path "target/gen/src"  :rules :cljs}
-                  {:source-paths ["test"] :output-path "target/gen/test" :rules :clj}
-                  {:source-paths ["test"] :output-path "target/gen/test" :rules :cljs}]}
-  :cljsbuild {:builds [{:source-paths ["target/gen/src"]
-                        :compiler {:output-to "target/gen/js/main.js"
+  :cljx {:builds [{:source-paths ["src"]  :output-path "target/classes"  :rules :clj}
+                  {:source-paths ["src"]  :output-path "target/classes"  :rules :cljs}
+                  {:source-paths ["test"] :output-path "target/test-classes" :rules :clj}
+                  {:source-paths ["test"] :output-path "target/test-classes" :rules :cljs}]}
+  :profiles {:dev {:plugins 
+    [[com.keminglabs/cljx "0.4.0" :exclusions [org.clojure/clojure]]
+     [lein-cljsbuild "1.0.3"]
+     [com.cemerick/clojurescript.test "0.3.1"]]
+     :aliases {"cleantest" ["do" "clean," "cljx" "once," "test," "cljsbuild" "test"]
+               "deploy" ["do" "clean," "cljx" "once," "deploy" "clojars"]}
+     :hooks [cljx.hooks]}}
+
+  
+  :source-paths ["src/cljx" "target/classes"]
+  :test-paths ["target/test-classes"]
+  
+  :cljsbuild {:builds [{:source-paths ["target/classes" "target/test-classes"]
+                        :compiler {:output-to "target/testable.js"
                                    :optimizations :advanced
-                                   :pretty-print false}}
-                       {:source-paths ["target/gen/src" "target/gen/test"]
-                        :compiler {:output-to "target/gen/test/js/testable.js"
-                                   :optimizations :simple
-                                   :pretty-print true}}
-                       {:source-paths ["target/gen/src"]
-                        :compiler {:output-to "target/gen/js/main.js"
-                                   :output-dir "target/gen/js"
-                                   :optimizations :whitespace
-                                   :pretty-print true
-                                   :source-map "target/gen/js/main.js.map"}}]
+                                   :pretty-print true}}]
+                       ;{:source-paths ["target/gen/src"]
+                       ; :compiler {:output-to "target/gen/js/main.js"
+                        ;           :output-dir "target/gen/js"
+                        ;           :optimizations :whitespace
+                        ;           :pretty-print true
+                        ;           :source-map "target/gen/js/main.js.map"}}]
               :test-commands {"phantomjs" ["phantomjs"
                                            :runner
                                            "test/bind-polyfill.js"
                                            "target/gen/test/js/testable.js"
                                            ]}}
-  :aliases {"build-once" ["do" "cljx" "once," "cljsbuild" "once"]
-            "deploy-lib" ["do" "build-once," "deploy" "clojars," "install"]}
+  
+  ;:aliases {"build-once" ["do" "cljx" "once," "cljsbuild" "once"]
+  ;          "deploy-lib" ["do" "build-once," "deploy" "clojars," "install"]}
   )
